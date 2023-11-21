6fafe6760d79e4203ca833f76f2cbffc6515ae59
mshradtya <adityapmishra196@gmail.com>
2023-11-21 20:55:55 +0530

    https

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://54.87.196.46:8010/api";
+const BASE_URL = "https://54.87.196.46:8010/api";
 // const BASE_URL = "http://localhost:8010/api";
 
 export default axios.create({
